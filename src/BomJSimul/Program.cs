e90b326f908b00fbfa1f2b8e9c741e3806a6cb55
Artem Saikin <art.saikin@gmail.com>
2020-11-22 19:57:14 +0400

    Initial commit

---
+++ src/BomJSimul/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BomJSimul
+{
+    using System;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
