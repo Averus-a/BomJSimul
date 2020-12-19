84855571554d2733090768d0016422b9a0c674fa
GEROM <ty.gerom@gmail.com>
2020-12-20 00:49:58 +0300

    Unit test для TryingToWinMoney

---
+++ src/BomJSimul/UnitTests/PlayerUsesSlotMachineTest.cs
@@ -0,0 +1,33 @@
+﻿namespace BomJSimul.UnitTests
+{
+    using BomJSimul.Entities;
+    using BomJSimul.GameLogic;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    [TestFixture]
+    public class PlayerUsesSlotMachineTest
+    {
+        [Test]
+        [TestCase(null, 10, 20, typeof(ArgumentException))]
+        internal void TryingToWinMoney(Player player, int rate, int maximumRate, Type expectedException)
+        {
+            Exception ex2 = null;
+            try
+            {
+                var slotMashin = new PlayerUsesSlotMachine();
+                slotMashin.TryingToWinMoney(player, rate, maximumRate);
+            }
+            catch(Exception ex)
+            {
+                ex2 = ex;
+            }
+
+            Assert.AreEqual(ex2?.GetType(), expectedException);
+        }
+
+
+    }
+}
